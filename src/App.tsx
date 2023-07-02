7aeb2d44aafcd2147b893daeb24bc985040aed7d
Vo7kov <jegor.volkov.a@gmail.com>
2023-07-02 23:42:55 +0300

    add validated  inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
+
+import { Form } from './Components/Form/Form';
+
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-}
+// export const App2 = () => {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm<Inputs>();
+//   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//       <input {...register('example', { required: true })} />
+//       {errors.example && <span>This field is required1</span>}
+
+//       <input {...register('myTest', { required: true })} />
+//       {errors.myTest && <span>This field is required2</span>}
+
+//       <button type="submit">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// };
+
+export const App: React.FC = () => (
+  <div className="App">
+    <Form />
+  </div>
 );
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
-    </div>
-  );
-};
---
+++ src/Components/Form/Form.tsx
@@ -0,0 +1,233 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
+import React, { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+import './Form.scss';
+
+type Inputs = {
+  firstName: string,
+  lastName: string,
+  email: string,
+  address: string,
+  zipCode: string,
+  phone: string,
+  country: { value: string; label: string } | null,
+};
+
+export const Form: React.FC = () => {
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = () => setIsSuccess(true);
+
+  return (
+    <>
+      <form
+        action="#"
+        method="POST"
+        className="form"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label className="form__label" htmlFor="firstName">
+          {errors.firstName && errors.firstName.type === 'required' && (
+            <p className="form__input-error">
+              First name is required
+            </p>
+          )}
+
+          {errors.firstName && errors.firstName.type === 'pattern' && (
+            <p className="form__input-error">
+              {errors.firstName.message}
+            </p>
+          )}
+
+          <input
+            className="form__input"
+            placeholder="First name"
+            id="firstName"
+            {...register('firstName', {
+              required: 'required',
+              pattern: {
+                value: /^[A-Za-z]+$/,
+                message: 'Please, enter only English letters without digits',
+              },
+            })}
+          />
+        </label>
+
+        <label className="form__label" htmlFor="lastName">
+          {errors.lastName && errors.lastName.type === 'required' && (
+            <p className="form__input-error">
+              First name is required
+            </p>
+          )}
+
+          {errors.firstName && errors.firstName.type === 'pattern' && (
+            <p className="form__input-error">
+              {errors.lastName?.message}
+            </p>
+          )}
+
+          <input
+            className="form__input"
+            placeholder="Last name"
+            id="lastName"
+            {...register('lastName', {
+              required: 'required',
+              pattern: {
+                value: /^[A-Za-z]+$/,
+                message: 'Please, enter only English letters without digits',
+              },
+            })}
+          />
+        </label>
+
+        <label className="form__label" htmlFor="email">
+          {errors.email && errors.email.type === 'required' && (
+            <p className="form__input-error">
+              Email is required
+            </p>
+          )}
+
+          {errors.email && errors.email.type === 'pattern' && (
+            <p className="form__input-error">
+              {errors.email?.message}
+            </p>
+          )}
+
+          {errors.email && errors.email.type === 'containsRussianLetters' && (
+            <p className="form__input-error">
+              Email should not contain russian letters
+            </p>
+          )}
+
+          <input
+            className="form__input"
+            placeholder="Email"
+            id="email"
+            {...register('email', {
+              required: 'required',
+              pattern: {
+                value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: 'Email did not match required format',
+              },
+              validate: {
+                containsRussianLetters: value => !/[\u0400-\u04FF]/.test(value),
+              },
+            })}
+          />
+        </label>
+
+        <label className="form__label" htmlFor="address">
+          {errors.address && errors.address.type === 'required' && (
+            <p className="form__input-error">
+              Adress is required
+            </p>
+          )}
+
+          {errors.address && errors.address.type === 'pattern' && (
+            <p className="form__input-error">
+              {errors.address?.message}
+            </p>
+          )}
+
+          <input
+            className="form__input"
+            placeholder="Address"
+            id="address"
+            {...register('address', {
+              required: 'required',
+              pattern: {
+                value: /^[A-Za-z]+$/,
+                message: 'Please, enter only English letters without digits',
+              },
+            })}
+          />
+        </label>
+
+        <label className="form__label" htmlFor="zipCode">
+          {errors.zipCode && errors.zipCode.type === 'required' && (
+            <p className="form__input-error">
+              ZIP Code is required
+            </p>
+          )}
+
+          {errors.zipCode && errors.zipCode.type === 'pattern' && (
+            <p className="form__input-error">
+              {errors.zipCode?.message}
+            </p>
+          )}
+
+          {errors.zipCode && errors.zipCode.type === 'format' && (
+            <p className="form__input-error">
+              ZIP Code must be 6 characters
+            </p>
+          )}
+
+          <input
+            className="form__input"
+            placeholder="ZIP Code"
+            id="zipCode"
+            {...register('zipCode', {
+              required: 'required',
+              pattern: {
+                value: /^[0-9]+$/,
+                message: 'Please, enter only digits',
+              },
+              validate: {
+                format: (value) => value.length === 6,
+              },
+            })}
+          />
+        </label>
+
+        <label className="form__label" htmlFor="phone">
+          {errors.phone && errors.phone.type === 'required' && (
+            <p className="form__input-error">
+              Phone number is required
+            </p>
+          )}
+
+          {errors.phone && errors.phone.type === 'pattern' && (
+            <p className="form__input-error">
+              {errors.phone?.message}
+            </p>
+          )}
+
+          <input
+            className="form__input"
+            placeholder="Phone number"
+            id="phone"
+            {...register('phone', {
+              required: 'required',
+              pattern: {
+                value: /^\d{3}-\d{4}$/,
+                message: 'Phone number did not match required format: 000-0000',
+              },
+            })}
+          />
+        </label>
+
+        <button
+          type="submit"
+          className="form__button"
+        >
+          Submit
+        </button>
+      </form>
+
+      {isSuccess && (
+        <p
+          className="form__success"
+        >
+          All field are correct! Well done!
+        </p>
+      )}
+    </>
+  );
+};
